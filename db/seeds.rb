5732f055ff9df754bbd78b1a5f1f082ae19df126
Giorgio Zuccarelli <giorgio.zuccarelli21@avenues.org>
2021-04-15 16:25:52 -0300

    finished everything regarding restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting...'
+Restaurant.destroy_all
+
+puts 'Generating new restaurants...'
+
+10.times do
+  restaurant = Restaurant.create(
+    {
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: %w[chinese italian japanese french belgian].sample
+    }
+  )
+  puts "Restaurant with ID: #{restaurant.id} was created!"
+end
